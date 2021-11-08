12b5b1aaf3c17b5890e510a97b60a9ff9c33fe59
ruy-sevalho <ruy.sevalho@gmail.com>
2021-11-08 19:00:35 -0300

    Refactor serializer function and add deserializer function

--- src/dataclass_tools/tools.py
+++ src/dataclass_tools/tools.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, Field, field, fields, is_dataclass
+from dataclasses import dataclass, Field, field, fields, is_dataclass, replace
 from typing import Any, Optional, Protocol, Union, get_args, runtime_checkable
 from copy import copy, deepcopy
 from enum import Enum
@@ -64,83 +64,169 @@ class DeSerializerOptions:
     overwrite_key: Optional[str] = None
 
 
-def _get_field_options(field_: Field) -> DeSerializerOptions:
-    return field_.metadata.get(METADATA_KEY, DeSerializerOptions())
+@dataclass
+class ToolsField:
+    field_: Field
 
+    def __post_init__(self):
+        self.options = self.field_.metadata.get(METADATA_KEY, DeSerializerOptions())
 
-def _get_type_str(type_name: Union[GetType, str], typ: type):
-    """Returns a string to name a type."""
+    @property
+    def _key(self):
+        return self.options.overwrite_key or self.field_.name
 
-    if isinstance(type_name, str):
-        return str
-    if isinstance(type_name, GetType):
-        return type_name(typ)
+    def _type_str(self, typ: type):
+        """Returns a string to name a type."""
+        name = self.options.type_name
 
-
-def _get_type_key(field_: Field):
-    """Returns a key for the type of a field, to be used in dictionary representation of dataclass."""
-
-    options = _get_field_options(field_)
-    name = options.overwrite_key or field_.name
-    # The default type label to be used depends the obj will be flattened or not
-    # This was my best way to do it so far, has room for improvement
-    default_label = DEFAULT_TYPE_LABEL if not options.flatten else name
-    return options.type_label or default_label
-
-
-def _get_and_process_value(obj, field_: Field):
-    """Returns the obj serialized as a dict. Should be called on individual instances, not collections."""
-
-    options = _get_field_options(field_)
-    # Attr should be a unique value, to be used as key in dictionary
-    if options.subs_by_attr:
-        attr = getattr(obj, options.subs_by_attr)
-        if not isinstance(attr, get_args(PERMITED_KEY_TYPES)):
-            raise KeyTypeError(
-                f"attr chosen to substitute dataclass value is assumed will be used as key in dictionary, so cannot be of '{type(attr).__name__}' type."
+        if isinstance(name, str):
+            return str
+        if isinstance(name, GetType):
+            return name(typ)
+        else:
+            raise ValueError(
+                """type_name option must be either a string or a callable that receives 
+                a type an returns a string.
+                """
             )
-        return getattr(obj, options.subs_by_attr)
-    value = _get_value(obj)
-    if options.add_type:
-        type_key = _get_type_key(field_)
-        type_str = _get_type_str(options.type_name, type(obj))
-        # Putting the type entry in the begging seemed to make more sense
-        value = {**{type_key: type_str}, **value}
-    return value
 
+    @property
+    def _type_key(self):
+        """Returns a key for the type of a field, to be used in dictionary representation of dataclass."""
 
-def _serialize_field(
-    obj: Any,
-    field_: Field,
-    in_list: bool = False,
-):
-    """Returns a dataclass field serialized."""
+        # The default type label to be used depends the obj will be flattened or not
+        # This was my best way to do it so far, has room for improvement
+        default_label = DEFAULT_TYPE_LABEL if not self.options.flatten else self._key
+        return self.options.type_label or default_label
 
-    options = _get_field_options(field_)
-    name = options.overwrite_key or field_.name
-    if isinstance(obj, (list, tuple)):
-        value: Union[list, tuple, dict] = type(obj)(
-            _serialize_field(item, field_, in_list=True) for item in obj
-        )
-        return {name: value}
-    elif isinstance(obj, dict):
-        value = dict()
-        for key, item in obj.items():
-            f: Field = field()
-            f.name = deepcopy(key)
-            value.update(**_serialize_field(item, f))
-    else:
-        value = _get_and_process_value(obj, field_)
-    if options.flatten or in_list:
+    def _get_and_process_value(self, obj):
+        """Returns the obj serialized as a dict. Should be called on individual instances,
+        not collections.
+        """
+
+        options = self.options
+        # Attr should be a unique value, to be used as key in dictionary
+        if options.subs_by_attr:
+            attr = getattr(obj, options.subs_by_attr)
+            if not isinstance(attr, get_args(PERMITED_KEY_TYPES)):
+                raise KeyTypeError(
+                    f"attr chosen to substitute dataclass value is assumed will be used as key in dictionary, so cannot be of '{type(attr).__name__}' type."
+                )
+            return getattr(obj, options.subs_by_attr)
+        value = _get_value(obj)
+        if options.add_type:
+            type_str = self._type_str(type(obj))
+            # Putting the type entry in the begging seemed to make more sense
+            value = {**{self._type_key: type_str}, **value}
         return value
-    return {name: value}
+
+    def _serialize_field(
+        self,
+        obj: Any,
+        in_list: bool = False,
+    ):
+        """Returns a dataclass field serialized."""
+
+        options = self.options
+        name = self._key
+        if isinstance(obj, (list, tuple)):
+            value: Union[list, tuple, dict] = type(obj)(
+                self._serialize_field(item, in_list=True) for item in obj
+            )
+            return {name: value}
+        elif isinstance(obj, dict):
+            value = dict()
+            for key, item in obj.items():
+                f: Field = replace(field(), name=deepcopy(key))
+                # f.name = deepcopy(key)
+                value.update(**ToolsField(f)._serialize_field(item))
+        else:
+            value = self._get_and_process_value(obj)
+        if options.flatten or in_list:
+            return value
+        return {name: value}
+
+    def _field_type(self, dct: dict):
+        typ = self.field_.type
+        if self.options.add_type:
+            if self.options.add_type and not self.options.subtype_table:
+                raise TypeError(
+                    "If add_type is specified for a field a subtype_table must be given also."
+                )
+            if self.options.flatten:
+                type_repr = dct[self._type_key]
+            else:
+                type_repr = dct[self._key][self._type_key]
+            table = self.options.subtype_table
+            typ = table[type_repr]
+        return typ
+
+    def _field_keys(self, dct: dict):
+        if self.options.flatten:
+            typ = self._field_type(dct)
+            if not isinstance(typ, DataClass):
+                raise (
+                    ValueError(
+                        f"Only flattened dataclass fields can be deseirialized, '{typ.__name__}' cannot"
+                    )
+                )
+            inner_keys = []
+            if self.options.add_type:
+                inner_keys.append(self._type_key)
+            for inner_field in fields(typ):
+                tools_inner_field = ToolsField(inner_field)
+                key = tools_inner_field._key
+                inner_dict = dct[key]
+                if tools_inner_field.options.flatten:
+                    inner_dict = dct
+                inner_keys.append(tools_inner_field._field_keys(inner_dict))
+            return inner_keys
+        return self._key
+
+    def _deserialize_field(self, raw_dct: dict, build_instance=False) -> dict[str, Any]:
+        """Deserializes a field instance."""
+
+        if hasattr(self.field_.type, "__origin__"):
+            origin: type = self.field_.type.__origin__
+            if origin == list or origin == tuple:
+                inner_type_field = replace(
+                    self.field_, type=self.field_.type.__agrs__[0]
+                )
+                value = origin(
+                    ToolsField(inner_type_field)._deserialize_field(item)
+                    for item in raw_dct[self._key]
+                )
+            elif origin == dict:
+                if self.options.flatten:
+                    raise (
+                        ValueError(
+                            f"Only flattened dataclass fields can be deseirialized, 'dict' cannot"
+                        )
+                    )
+                inner_type_field = replace(
+                    self.field_, type=self.field_.type.__agrs__[1]
+                )
+                value = {
+                    key: ToolsField(inner_type_field)._deserialize_field(raw_dct=value)
+                    for key, value in raw_dct[self._key].items()
+                }
+        value = raw_dct[self._key]
+        if self.options.flatten:
+            field_keys = self._field_keys(raw_dct)
+            input_dict = {key: raw_dct[key] for key in field_keys}
+            value = deserialize_dataclass(input_dict, self._field_type)
+        typ = self._field_type(raw_dct)
+        if is_dataclass(typ):
+            value = deserialize_dataclass(value, typ)
+        return {self.field_.name: value}
 
 
 def _serialize_dataclass(obj: DataClass) -> dict[str, Any]:
     """Returns a serilized dataclass object."""
 
     list_of_dict_repr = [
-        _serialize_field(getattr(obj, field_.name), field_) for field_ in fields(obj)
+        ToolsField(field_)._serialize_field(getattr(obj, field_.name))
+        for field_ in fields(obj)
     ]
     return {key: value for x in list_of_dict_repr for key, value in x.items()}
 
@@ -163,7 +249,7 @@ def serialize_dataclass(
 
     if not isinstance(obj, DataClass):
         raise TypeError(f"obj must be a dataclass, not '{type(obj)}'")
-    return _get_value(obj)
+    return _serialize_dataclass(obj)
 
 
 def deserialize_dataclass(
@@ -188,73 +274,6 @@ def _deserialize_dataclass(dct: dict, dataclass: DataClass):
     """Derializes a dataclass instance."""
 
     list_of_input_dict = [
-        _deserialize_field(field_, dct) for field_ in fields(dataclass)
+        ToolsField(field_)._deserialize_field(dct) for field_ in fields(dataclass)
     ]
     return {key: value for x in list_of_input_dict for key, value in x.items()}
-
-
-def _get_field_type(field_: Field, dct: dict):
-    options = _get_field_options(field_)
-    name = options.overwrite_key or field_.name
-    typ = field_.type
-    if options.add_type:
-        type_key = _get_type_key(field_)
-        if options.flatten:
-            type_repr = dct[type_key]
-        else:
-            type_repr = dct[name][type_key]
-        table = options.subtype_table
-        typ = table[type_repr]
-    return typ
-
-
-def _get_field_keys(field_: Field):
-    options: DeSerializerOptions = _get_field_options(field_)
-    typ = _get_field_type()
-    if options.flatten:
-        inner_keys = _get_field_keys()
-
-    return
-
-
-def _deserialize_options_check(options: DeSerializerOptions):
-    if options.add_type and not options.subtype_table:
-        raise TypeError(
-            "If add_type is specified for a field a subtype_table must be given also."
-        )
-    return
-
-
-def _deserialize_field(field_: Field, raw_dct: dict):
-    """Deserializes a field instance."""
-
-    options = _get_field_options(field_)
-    # Some combinations of options values don't make sense together so they are checked here
-    _deserialize_options_check(options)
-    name = options.overwrite_key or field_.name
-    if hasattr(field_.type, "__origin__"):
-        origin: type = field_.type.__origin__
-        if origin == list or origin == tuple:
-            inner_type_field = copy(field_)
-            inner_type_field: type = field_.type.__agrs__[0]
-            value = origin(
-                _deserialize_field(inner_type_field, item) for item in raw_dct[name]
-            )
-        elif origin == dict:
-            value = dict()
-    typ = _get_field_type(field_, raw_dct)
-
-    input_dict = raw_dct[name]
-
-    if options.flatten:
-        field_keys = _get_field_keys(field_)
-        input_dict = {key: raw_dct[key] for key in field_keys}
-
-    if is_dataclass(typ):
-        value = _deserialize_dataclass(input_dict, typ)
-
-    return {field_.name: value}
-
-
-def _deserialize_value(field_: Field, obj):
-    return
