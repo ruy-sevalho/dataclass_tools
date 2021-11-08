17b965f4800a5b4aee145e239d662e64c1de0197
ruy-sevalho <ruy.sevalho@gmail.com>
2021-11-08 18:59:23 -0300

    Factor out example classes in tests as fixtures

--- test/test_.py
+++ test/test_.py
@@ -1,34 +1,40 @@
 from dataclasses import asdict, dataclass
-
+from pytest import fixture
 from hypothesis import given, strategies as st
 
 from dataclass_tools.tools import serialize_dataclass
 
 
-@given(st.floats(), st.characters())
-def test_float_str_flat_strucuture(age, name):
+@fixture
+def child():
     @dataclass
     class Child:
         age: float
         name: str
 
-    child = Child(age=age, name=name)
-    assert serialize_dataclass(child) == asdict(child)
+    return Child
 
 
-@given(st.tuples(st.tuples(st.floats(), st.characters())), st.characters())
-def test_float_str_nested_strucuture(childs_tuples, name_parent):
-    @dataclass
-    class Child:
-        age: float
-        name: str
-
+@fixture
+def parent(child):
     @dataclass
     class Parent:
-        childs: list[Child]
+        childs: list[child]
         name: str
 
-    childs = tuple(Child(age=age, name=name_child) for age, name_child in childs_tuples)
-    parent = Parent(childs=childs, name=name_parent)
+
+@given(st.floats(), st.characters())
+def test_float_str_flat_strucuture(age, name, child):
+    child_instance = child(age=age, name=name)
+    assert serialize_dataclass(child_instance) == asdict(child_instance)
+
+
+@given(
+    st.tuples(st.tuples(st.floats(), st.characters())), st.characters(), child, parent
+)
+def test_float_str_nested_strucuture(childs_tuples, name_parent):
+
+    childs = tuple(child(age=age, name=name_child) for age, name_child in childs_tuples)
+    parent_instance = parent(childs=childs, name=name_parent)
 
     assert serialize_dataclass(parent) == asdict(parent)
